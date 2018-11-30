d910d62eb4b00915edcf5a33f88b83542f20d17c
ccjie0313 <tinofmail@gmail.com>
2018-11-30 19:55:18 +0900

    Revert "Initial commit"
    This reverts commit c3a7c75330d05bcd9e9ff47c1bbff832f6805431.

--- 2018_1124/app/const/const.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-/** バージョン */
-const VERSION = '1.1';
-
-const DEFAULT_HTML = '/html/index.html.php';
-
--- app/const/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-  const DB_CONFIG = [
-     ip => 'mysql723.db.sakura.ne.jp',
-     name => 'mytop',
-     pwd => 'e5na8sn5mp',
-     db => 'mytop_01',
-     charset =>'utf-8'
- ];
